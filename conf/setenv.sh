fa87100a9eef49fbefb4ef7df00930b8b290f263
Laurent REMY <lremy@is4ri.com>
2019-09-02 16:49:14 +0200

    update options with lfr7.2

--- conf/setenv.sh
+++ conf/setenv.sh
@@ -1,9 +1,8 @@
-
-JAVA_OPTS="-Djava.awt.headless=true -XX:+UseG1GC -Dfile.encoding=UTF8 -Duser.timezone=Europe/Paris $JAVA_OPTS " # java base settings
-JAVA_OPTS="-XX:MaxPermSize=512M -Xms1024M -Xmx2048M $JAVA_OPTS " # java-memory-settings
-
+CATALINA_OPTS="-Djava.locale.providers=JRE,COMPAT,CLDR"
+CATALINA_OPTS="$CATALINA_OPTS -Djava.awt.headless=true -XX:+UseG1GC -Duser.timezone=Europe/Paris" # java base settings
+CATALINA_OPTS="$CATALINA_OPTS -XX:MaxPermSize=512M -Xms2048M -Xmx2048M" # java-memory-settings
 CATALINA_OPTS="$CATALINA_OPTS -Dfile.encoding=UTF8 -Djava.net.preferIPv4Stack=true  -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false"
 CATALINA_OPTS="$CATALINA_OPTS -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=/tmp/dump.hprof" # provide dump on OutOfMemoryError
 CATALINA_OPTS="$CATALINA_OPTS -Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=9901 -Dcom.sun.management.jmxremote.rmi.port=9901 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false -Djava.rmi.server.hostname=localhost" # JMX settings
 
-
+# CATALINA_OPTS="-XX:MaxNewSize=1536m -XX:MaxMetaspaceSize=512m -XX:MetaspaceSize=512m -XX:NewSize=1536m -XX:SurvivorRatio=7"
